aba96df5f6aa574fff168e99055d134455adfaa2
Y Chen <yafeng_chen@outlook.com>
2020-12-13 21:39:35 +0100

    Ninja

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,149 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <lable>Email:</lable>
+    <input type="email" required v-model="email" />
+    <lable>Password:</lable>
+    <input type="password" required v-model="password" />
+    <div class="error" v-if="passwordError">{{ passwordError }}</div>
+
+    <label>Role:</label>
+    <select v-model="role">
+      <option value="developer">Web Developer</option>
+      <option value="designer">Web Designer</option>
+    </select>
+
+    <label>Skills:</label>
+    <input type="text" v-model="tempSkill" @keyup.alt="addSkill" />
+    <div v-for="skill in skills" :key="skill" class="pill">
+      <span @click="deleteSkill(skill)">{{ skill }}</span>
+    </div>
+
+    <div class="terms">
+      <input type="checkbox" v-model="terms" required />
+      <label>Accept terms and conditions</label>
+    </div>
+
+    <div class="submit">
+      <button>Create an Account</button>
+    </div>
+
+    <!-- <div>
+      <input type="checkbox" value="zhu" v-model="names" />
+      <label>Zhu</label>
+    </div>
+    <div>
+      <input type="checkbox" value="yi" v-model="names" />
+      <label>Yi</label>
+    </div>
+    <div>
+      <input type="checkbox" value="long" v-model="names" />
+      <label>Long</label>
+    </div> -->
+  </form>
+</template>
+>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+      role: "",
+      terms: false,
+      //names: [],
+      tempSkill: "",
+      skills: [],
+      passwordError: "",
+    };
+  },
+  methods: {
+    addSkill(e) {
+      if (e.key === "," && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill);
+        }
+        this.tempSkill = "";
+      }
+    },
+    deleteSkill(skill) {
+      this.skills = this.skills.filter((s) => s !== skill);
+    },
+    handleSubmit() {
+      this.passwordError =
+        this.password.length > 5
+          ? ""
+          : "Password must be at least 6 chars long";
+      if (!this.passwordError) {
+        alert("Form Submitted!");
+      }
+    },
+  },
+};
+</script>
+
+<style>
+form {
+  max-width: 420px;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
+}
+label {
+  color: #aaa;
+  display: inline-block;
+  margin: 25px 0 15px;
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
+}
+input,
+select {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
+}
+input[type="checkbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
+}
+.pill {
+  display: inline-block;
+  margin: 20px 10px 0 0;
+  padding: 6px 12px;
+  background: #eee;
+  border-radius: 20px;
+  font-size: 12px;
+  letter-spacing: 1px;
+  font-weight: bold;
+  color: #777;
+  cursor: pointer;
+}
+button {
+  background: #0b6dff;
+  border: 0;
+  padding: 10px 20px;
+  margin-top: 20px;
+  color: white;
+  border-radius: 20px;
+}
+.submit {
+  text-align: center;
+}
+.error {
+  color: #ff0062;
+  margin-top: 10px;
+  font-size: 0.8em;
+  font-weight: bold;
+}
+</style>
